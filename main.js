325f68c9c6b5b060f2e8bb3b43f9c06363071d99
mpaternostro <marcelodpaternostro@gmail.com>
2019-12-01 23:40:42 -0300

    Arregla un par de cosas, pero aun NO ANDA
    Hay que arreglar los intervalos en los que se prenden y apagan las esquinas.

--- main.js
+++ main.js
@@ -1,13 +1,68 @@
+const topLeft = document.querySelector('#top-left');
+const topRight = document.querySelector('#top-right');
+const bottomLeft = document.querySelector('#bottom-left');
+const bottomRight = document.querySelector('#bottom-right');
+const corners = [topLeft, topRight, bottomLeft, bottomRight];
+let chosenCorners = [];
+
 document.querySelector('.start').onclick = () => {
-    const topLeft = document.querySelector('#top-left');
-    const originalColor = 'border-top: 100px solid #0c9463;';
-    const newColor = 'border-top: 100px solid #6decb9;';
-    topLeft.style = newColor;
-    setTimeout(() => {
-        topLeft.style = originalColor;
-    }, 1500);
+    pickRandomCorner(chosenCorners);
+    playSequence(chosenCorners);
 }
 
-// 1. Hacer que original color tome el color original del lugar que toco al azar
-// 2. El new color tiene que estar predefinido en algun lugar
-// 3. Cuando cambia al new color tiene que sombrearlo tambien (capaz hay que ponerle un clase predefinida, y despues del timeout volver a la clase por default de ese lado)
+function pickRandomCorner(chosenCorners) {
+    let corner = randomCorner(corners);
+    return chosenCorners.push(corner);
+}
+
+function randomCorner(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+function playSequence(chosenCorners) {
+    const originalColors = obtainOriginalColors(chosenCorners);
+    const newColors = obtainNewColors();
+    console.log(originalColors);
+
+    chosenCorners.forEach((element, index) => {
+        const corner = element.id;
+        const delay = (index + 1) * 1000;
+        setTimeout(() => {
+            element.style['border-top-color'] = newColors[corner];
+        }, delay);
+        setTimeout(() => {
+            element.style['border-top-color'] = originalColors[corner];
+        }, delay + 1000);
+    });
+}
+
+function obtainOriginalColors(chosenCorners) {
+    const originalColors = [];
+    chosenCorners.forEach(element => {
+        const color = getComputedStyle(element)['border-top-color'];
+        originalColors[element.id] = color;
+    });
+
+    return originalColors;
+}
+
+function obtainNewColors() {
+    const newGreen = '#009900';
+    const newRed = '#FF0000';
+    const newYellow = '#FFFF00';
+    const newBlue = '#0000FF';
+    const newColorsPreview = [newGreen, newRed, newYellow, newBlue];
+
+    const topLeft = document.querySelector('#top-left');
+    const topRight = document.querySelector('#top-right');
+    const bottomLeft = document.querySelector('#bottom-left');
+    const bottomRight = document.querySelector('#bottom-right');
+    const corners = [topLeft, topRight, bottomLeft, bottomRight];
+
+    const newColors = [];
+    corners.forEach((element, index) => {
+        newColors[element.id] = newColorsPreview[index];
+    });
+
+    return newColors;
+}
